22a7462a93d980bfd06e15a9512aa68541b036bd
Macaronious <ammu1000@stud.ek.dk>
2026-04-16 19:02:55 +0200

    initial commit

---
+++ src/main/java/com/Ammar/wishlistapp/WishlistAppApplication.java
@@ -0,0 +1,15 @@
+package com.Ammar.wishlistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistAppApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WishlistAppApplication.class, args);
+    }
+
+}
