d13bd7b036efe114dd0b9332f14bf43c2e793091
Macaronious <ammu1000@stud.ek.dk>
2026-04-21 22:56:38 +0200

    Azure

--- src/main/java/com/Ammar/wishlistapp/WishlistAppApplication.java
+++ src/main/java/com/Ammar/wishlistapp/WishlistAppApplication.java
@@ -2,9 +2,10 @@ package com.Ammar.wishlistapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class WishlistAppApplication
 {
 
